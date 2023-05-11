ef83de94c86ef67c52bf005b5b517cc5890284b5
Neo Maraisane <maraisanen@gmail.com>
2023-05-11 12:56:27 -0400

    Created limited access permissions to resources

--- lib/stacks/iam/iam-roles-stack.ts
+++ lib/stacks/iam/iam-roles-stack.ts
@@ -13,78 +13,47 @@ export class IamRolesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // const dynamodbStreamArn = StringParameter.fromStringParameterAttributes(
-    //   this,
-    //   "StringParameter",
-    //   {
-    //     parameterName: "/dynamodb/table/arn",
-    //   }
-    // ).stringValue;
+    const dataProcessingLambdaPermissions = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "firehose:PutRecord",
+        "ssm:GetParameter",
+      ],
+      resources: ["*"],
+    });
 
-    // const lambdaDynamoDBPermissions = new PolicyStatement({
-    //   effect: Effect.ALLOW,
-    //   actions: [
-    //     "dynamodb:GetRecords",
-    //     "dynamodb:GetShardIterator",
-    //     "dynamodb:DescribeStream",
-    //     "dynamodb:ListStreams",
-    //   ],
-    //   resources: [dynamodbStreamArn],
-    // });
+    const kinesisFirehoseS3Permissions = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        "s3:AbortMultipartUpload",
+        "s3:GetBucketLocation",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:PutObject",
+        "lambda:InvokeFunction",
+        "lambda:GetFunctionConfiguration",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+      ],
+      resources: ["*"],
+    });
 
-    // const CloudWatchPermissions = new PolicyStatement({
-    //   effect: Effect.ALLOW,
-    //   actions: [
-    //     "logs:CreateLogGroup",
-    //     "logs:CreateLogStream",
-    //     "logs:PutLogEvents",
-    //   ],
-    //   resources: ["*"],
-    // });
-
-    // const lambdaKinesisFirehosePermissions = new PolicyStatement({
-    //   effect: Effect.ALLOW,
-    //   actions: [
-    //     "kinesis:DescribeStream",
-    //     "kinesis:DescribeStreamSummary",
-    //     "kinesis:GetRecords",
-    //     "kinesis:GetShardIterator",
-    //     "kinesis:ListShards",
-    //     "kinesis:ListStreams",
-    //     "kinesis:SubscribeToShard",
-    //   ],
-    //   resources: [dynamodbStreamArn],
-    // });
-
-    // const kinesisFirehoseS3Permissions = new PolicyStatement({
-    //   effect: Effect.ALLOW,
-    //   actions: [
-    //     "s3:AbortMultipartUpload",
-    //     "s3:GetBucketLocation",
-    //     "s3:GetObject",
-    //     "s3:ListBucket",
-    //     "s3:ListBucketMultipartUploads",
-    //     "s3:PutObject",
-    //   ],
-    //   resources: [], // bucket arn here
-    // });
-
-    // const kinesisFirehoseStreamPermissions = new PolicyStatement({
-    //   effect: Effect.ALLOW,
-    //   actions: [
-    //     "kinesis:DescribeStream",
-    //     "kinesis:GetShardIterator",
-    //     "kinesis:GetRecords",
-    //     "kinesis:ListShards",
-    //   ],
-    //   resources: [], // arn for kinesis stream here
-    // });
-
-    // const kinesisFirehoseLambdaPermissions = new PolicyStatement({
-    //   effect: Effect.ALLOW,
-    //   actions: ["lambda:InvokeFunction", "lambda:GetFunctionConfiguration"],
-    //   resources: [], // arn for lambda function here
-    // });
+    const redshiftLambdaPermissions = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        "ssm:GetParameter",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "lambda:DeleteFunction",
+      ],
+      resources: ["*"],
+    });
 
     const dataProcessingLambdaRole = new Role(
       this,
@@ -108,45 +77,9 @@ export class IamRolesStack extends Stack {
       description: "IAM role for kinesis firehose",
     });
 
-    redshiftLambdaRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AmazonRedshiftDataFullAccess")
-    );
-
-    redshiftLambdaRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMReadOnlyAccess")
-    );
-    redshiftLambdaRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("CloudWatchLogsFullAccess")
-    );
-    redshiftLambdaRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AWSLambda_FullAccess")
-    );
-
-    dataProcessingLambdaRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AmazonDynamoDBFullAccess")
-    );
-    dataProcessingLambdaRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("CloudWatchLogsFullAccess")
-    );
-    dataProcessingLambdaRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AmazonKinesisFirehoseFullAccess")
-    );
-    dataProcessingLambdaRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMReadOnlyAccess")
-    );
-
-    kinesisFirehoseRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess")
-    );
-    kinesisFirehoseRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AWSLambda_FullAccess")
-    );
-    kinesisFirehoseRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AmazonRedshiftQueryEditor")
-    );
-    kinesisFirehoseRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("CloudWatchLogsFullAccess")
-    );
+    redshiftLambdaRole.addToPolicy(redshiftLambdaPermissions);
+    dataProcessingLambdaRole.addToPolicy(dataProcessingLambdaPermissions);
+    kinesisFirehoseRole.addToPolicy(kinesisFirehoseS3Permissions);
 
     new StringParameter(this, "LambdaRoleStringParameter", {
       parameterName: "/iamrole/lambda/dataprocessing/arn",
@@ -165,14 +98,5 @@ export class IamRolesStack extends Stack {
       description: "Kinesis Firehose execution role arn",
       stringValue: redshiftLambdaRole.roleArn,
     });
-
-    // lambdaRole.addToPolicy(lambdaDynamoDBPermissions);
-    // lambdaRole.addToPolicy(lambdaKinesisFirehosePermissions);
-    // lambdaRole.addToPolicy(CloudWatchPermissions);
-
-    // kinesisFirehoseRole.addToPolicy(kinesisFirehoseS3Permissions);
-    // kinesisFirehoseRole.addToPolicy(kinesisFirehoseStreamPermissions);
-    // kinesisFirehoseRole.addToPolicy(kinesisFirehoseLambdaPermissions);
-    // kinesisFirehoseRole.addToPolicy(CloudWatchPermissions);
   }
 }
